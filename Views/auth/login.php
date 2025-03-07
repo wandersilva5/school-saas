3a5499cdcb2510c7e1a3daa538ed2def67d48afc
Wander <wandersilva5@gmail.com>
2025-03-07 00:12:06 -0300

    ajuste cor css login

--- Views/auth/login.php
+++ Views/auth/login.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
-            background: linear-gradient(135deg, #154A9A 0%, #1e6ddf 100%);
+            background: linear-gradient(135deg, #2e59d9 0%, #2e59d9 100%);
             min-height: 100vh;
             display: flex;
             align-items: center;
